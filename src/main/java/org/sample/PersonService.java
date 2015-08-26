2ec8e55a77991778204670d5ccba0b9bb664cdae
Humberto Streb <hstreb@gmail.com>
2015-08-25 23:06:48 -0300

    added some actions in PersonService and added jacoco plugin

--- src/main/java/org/sample/PersonService.java
+++ src/main/java/org/sample/PersonService.java
@@ -1,15 +1,28 @@
 package org.sample;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PersonService {
-    private Map<Integer, Person> people = new HashMap<Integer, Person>();
+	private Map<Integer, Person> people = new HashMap<Integer, Person>();
 
-    public Person add(Person person) {
-        int id = people.keySet().stream().max(Integer::compare).orElse(0) + 1;
-        person.setId(id);
-        people.put(person.getId(), person);
-        return person;
-    }
+	public Person add(Person person) {
+		int id = people.keySet().stream().max(Integer::compare).orElse(0) + 1;
+		person.setId(id);
+		people.put(person.getId(), person);
+		return person;
+	}
+
+	public Collection<Person> list() {
+		return people.values();
+	}
+
+	public boolean delete(Integer id) {
+		if (people.containsKey(id)) {
+			people.remove(id);
+			return true;
+		}
+		return false;
+	}
 }
--- src/test/java/org/sample/PersonServiceTest.java
+++ src/test/java/org/sample/PersonServiceTest.java
@@ -2,19 +2,50 @@ package org.sample;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsNull.nullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 import org.junit.Test;
 
 public class PersonServiceTest {
 
-    @Test
-    public void testAddFirstPersonWithIdEqualsOne() {
-        Person person = new Person("Hommer Simpson", 41); 
-        assertThat(person.getId(), nullValue());
-        PersonService service = new PersonService();
-        Person personInserted = service.add(person);
-        assertThat(personInserted.getId(), equalTo(1));
-    }
+	@Test
+	public void testAddFirstPersonWithIdEqualsOne() {
+		PersonService service = new PersonService();
+		Person personInserted = service.add(new Person("Hommer Simpson", 41));
+		assertThat(personInserted.getId(), equalTo(1));
+	}
+
+	@Test
+	public void testListReturnsEmpty() {
+		PersonService service = new PersonService();
+		assertThat(service.list(), hasSize(0));
+	}
+
+	@Test
+	public void testAddAPersonIncraseListSize() {
+		PersonService service = new PersonService();
+		service.add(new Person("Hommer Simpson", 41));
+		assertThat(service.list(), hasSize(1));
+		service.add(new Person("Margie Simpson", 38));
+		assertThat(service.list(), hasSize(2));
+	}
+
+	@Test
+	public void testDeleteAnInexistentPersonReturnsFalse() {
+		PersonService service = new PersonService();
+		service.add(new Person("Hommer Simpson", 41));
+		assertThat(service.list(), hasSize(1));
+		assertThat(service.delete(2), equalTo(false));
+		assertThat(service.list(), hasSize(1));
+	}
+
+	@Test
+	public void testDeleteAnExistentPersonReturnsTrue() {
+		PersonService service = new PersonService();
+		service.add(new Person("Hommer Simpson", 41));
+		assertThat(service.list(), hasSize(1));
+		assertThat(service.delete(1), equalTo(true));
+		assertThat(service.list(), hasSize(0));
+	}
 
 }
